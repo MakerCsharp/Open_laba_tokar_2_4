dbd742e95b4560d01f12ad8cb0bd78cc2e663fb1
MakerCsharp <61178402+MakerCsharp@users.noreply.github.com>
2020-09-20 21:15:56 +0300

    Добавьте файлы проекта.

---
+++ LABA_TOKAr_2_4/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LABA_TOKAr_2_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AdventureWorksLT2008REntities dataEntities = new AdventureWorksLT2008REntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var query =
+            from Customer in dataEntities.Customer
+            where Customer.Title == "Mr."
+            orderby Customer.Title
+            select new { Customer.FirstName,Customer.MiddleName,Customer.LastName};
+
+            dataGrid1.ItemsSource = query.ToList();
+        }
+    }
+}
